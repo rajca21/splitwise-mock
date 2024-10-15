3841dbb01b2c0b6ef2191e9e3fdf860ef18b17a6
rajca21 <2nikolar1@gmail.com>
2024-10-15 23:19:20 +0200

    updates for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 import { useAuthStore } from './store/authStore';
@@ -140,6 +140,8 @@ function App() {
             </ProtectedRoute>
           }
         />
+
+        <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
 
       <Toaster position='top-center' reverseOrder={false} />
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
@@ -11,6 +12,7 @@ import transactionRoutes from './routes/transaction.route.js';
 
 dotenv.config();
 const port = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 const app = express();
 app.use(
@@ -31,6 +33,13 @@ app.use('/api/users', userRoutes);
 app.use('/api/trips', tripRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/transactions', transactionRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   connectDB();
