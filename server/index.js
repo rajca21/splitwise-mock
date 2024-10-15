972f73e02d77e6b31a6fb07152be959b9233d622
rajca21 <2nikolar1@gmail.com>
2024-10-16 00:30:04 +0200

    build for npx create-react-app instead of vite

--- server/index.js
+++ server/index.js
@@ -35,9 +35,9 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/transactions', transactionRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/dist')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
